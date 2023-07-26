657c875fbd3d29322302bf63bc6b2e2595715aec
olexii ponomarenko <deka.pon.y@gmail.com>
2023-07-27 02:37:30 +0300

    init

---
+++ popup.js
@@ -0,0 +1,77 @@
+function InjectChecker() {
+  chrome.tabs.query({active: true}, function(tabs) {
+    var tab = tabs[0];
+    if (tab) {
+
+        let currentUrl = (new URL(tab.url));
+        //alert(currentUrl.protocol);
+        if (currentUrl.protocol !== "chrome:") {
+          chrome.scripting.executeScript(
+            {
+                target:{tabId: tab.id, allFrames: false},
+                func:checkPage
+            },
+            onChecked
+        )
+        }
+    } else {
+        alert("There are no active tabs");
+        return "no host";
+    }
+  })
+}
+
+function checkPage() {
+
+  let gres = document.querySelectorAll("div:not([id])>div.g>div>div>div a[ping][data-ved]");
+  let gimg = document.querySelectorAll("#islrg>div>div>a:nth-child(3)");
+  let gvideo = document.querySelectorAll("#search a[data-ved][jsname]");
+  let yres = document.querySelectorAll("ul>li>div>div>a:has(h2)");
+
+  let x;
+  if (gres.length>0) {
+    x= gres;
+  } else if (gimg.length>0) {
+    x= gimg;
+  } else if (gvideo.length>0) {
+    x= gvideo;
+  } else if (yres.length>0) {
+    x= yres;
+  } else {
+    x= [];
+  }
+
+  let res = "";
+  let resn = "";
+  let counter = 0+0;
+  x.forEach((el) => {
+  if (el.href.length>1 && el.offsetWidth>0) {
+    counter++;
+    resn = resn + counter + "\t" + el.href + "\n";
+    res = res + el.href + "\n";
+  };
+  });
+
+  return {
+    list:res,
+    listn:resn,
+  };
+} 
+
+let res;
+function onChecked(results) {
+  if (results) {
+    res = results[0].result;
+    if (res) {
+      document.querySelector("#list").innerHTML = res.list;
+    }
+  }
+
+}
+
+const grabBtn = document.getElementById("copy");
+grabBtn.addEventListener("click",() => {    
+  navigator.clipboard.writeText(res.listn);
+})
+
+InjectChecker();
